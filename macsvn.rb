48f12d42538f7273d67cec660c3269b6d05ff989
bkdevops <bkdevops@Mac-2836.local>
2024-09-12 17:47:59 +0800

    update

--- macsvn.rb
+++ macsvn.rb
@@ -5,7 +5,7 @@ class Macsvn < Formula
   desc "Subversion with pristine on demand"
   homepage ""
   url "https://code-res-1257584459.cos.ap-guangzhou.myqcloud.com/SVN/MacSVN/subversion-1.15.0.tar.xz"
-  sha256 "1ecfd02924fff74048ec7412c292ac9c906b8a3b28b6f03371987eab67eff7d1"
+  sha256 "eb9e35da4ff43d7670072abbcbc646b2ddc5097688bae8c79983ca538c60dfe1"
   license ""
 
   depends_on "serf"
