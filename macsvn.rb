80607b0f70ddf430a8eaa528d5cf54181aa69d7d
xhc2333 <56311726+xhc2333@users.noreply.github.com>
2024-09-12 15:28:57 +0800

    Update macsvn.rb

--- macsvn.rb
+++ macsvn.rb
@@ -5,13 +5,8 @@ class Macsvn < Formula
   desc "Subversion with pristine on demand"
   homepage ""
   url "https://code-res-1257584459.cos.ap-guangzhou.myqcloud.com/SVN/MacSVN/subversion-1.15.0.tar.xz"
-  sha256 ""
+  sha256 "7319d58bb44d29bdac0bf662d1e02d6f45b810f975f963c15280ecdeaea08976"
   license ""
-
-  depends_on "serf"
-  depends_on "apr"
-  depends_on "apr-util"
-  depends_on "zlib"
 
   def install
     bin.install "svn"
