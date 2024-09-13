74f3d04979e6de46c406074a45df4642d222c0b6
xhc2333 <56311726+xhc2333@users.noreply.github.com>
2024-09-13 12:20:21 +0800

    Update macsvn.rb

--- macsvn.rb
+++ macsvn.rb
@@ -24,12 +24,21 @@ class Macsvn < Formula
     system "install_name_tool", "-change", "/usr/local/opt/zlib/lib/libz.1.dylib", "#{lib_path}/zlib/lib/libz.1.dylib", "#{bin_path}/svn"
     system "install_name_tool", "-change", "/usr/local/opt/apr-util/lib/libaprutil-1.0.dylib", "#{lib_path}/apr-util/lib/libaprutil-1.0.dylib", "#{bin_path}/svn"
     system "install_name_tool", "-change", "/usr/local/opt/apr/lib/libapr-1.0.dylib", "#{lib_path}/apr/lib/libapr-1.0.dylib", "#{bin_path}/svn"
-    system "install_name_tool", "-change", "/usr/local/opt/openssl@3/lib/libssl.3.dylib", "#{lib_path}/openssl/lib/libssl.3.dylib", "#{bin_path}/svn"
 
-    system "install_name_tool", "-change", "/usr/local/opt/apr-util/lib/libaprutil-1.0.dylib", "#{lib_path}/apr-util/lib/libaprutil-1.0.dylib", "#{lib_path}/serf/lib/libserf-1.dylib"
-    system "install_name_tool", "-change", "/usr/local/opt/apr/lib/libapr-1.0.dylib", "#{lib_path}/apr/lib/libapr-1.0.dylib", "#{lib_path}/serf/lib/libserf-1.dylib"
-    system "install_name_tool", "-change", "/usr/local/opt/zlib/lib/libz.1.dylib", "#{lib_path}/zlib/lib/libz.1.dylib", "#{lib_path}/serf/lib/libserf-1.dylib"
-    system "install_name_tool", "-change", "/usr/local/opt/openssl@3/lib/libssl.3.dylib", "#{lib_path}/openssl/lib/libssl.3.dylib", "#{lib_path}/serf/lib/libserf-1.dylib"
-    system "install_name_tool", "-change", "/usr/local/opt/openssl@3/lib/libssl.3.dylib", "#{lib_path}/openssl/lib/libssl.3.dylib", "#{lib_path}/serf/lib/libserf-1.1.3.0.dylib"
+    files_to_update = Dir["#{lib_path}/serf/lib/*.dylib"]
+    files_to_update.each do |file|
+      dependencies = `otool -L #{file}`.split("\n").map(&:strip)
+      dependencies.each do |dep|
+        next if dep == file
+        old_path = dep.split(" ").first
+        if old_path.start_with?("/usr/local/opt")
+          new_path = old_path.sub("/usr/local/opt", "#{lib_path}")
+          system "install_name_tool", "-change", old_path, new_path, file
+        elsif old_path.start_with?("/usr/local/opt/openssl@3")
+          new_path = old_path.sub("/usr/local/opt/openssl@3", "#{lib_path}/openssl")
+          system "install_name_tool", "-change", old_path, new_path, file
+        end
+      end
+    end
   end
 end
