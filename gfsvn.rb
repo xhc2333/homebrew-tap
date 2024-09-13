fd5795a535b132dc50153d655575941612214287
xhc2333 <56311726+xhc2333@users.noreply.github.com>
2024-09-13 16:10:09 +0800

    Update and rename macsvn.rb to gfsvn.rb

--- gfsvn.rb
+++ gfsvn.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Macsvn < Formula
+class Gfsvn < Formula
   desc "Subversion with pristine on demand"
   homepage ""
   url "https://code-res-1257584459.cos.ap-guangzhou.myqcloud.com/SVN/MacSVN/subversion-1.15.0.tar.xz"
