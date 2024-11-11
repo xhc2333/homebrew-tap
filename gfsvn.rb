4edc5f06b9810778800078a461771af7a91dcc2d
xhc2333 <56311726+xhc2333@users.noreply.github.com>
2024-11-11 11:02:53 +0800

    Update gfsvn.rb

--- gfsvn.rb
+++ gfsvn.rb
@@ -1,6 +1,10 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require 'socket'
+require 'open3'
+require 'json'
+
 class Gfsvn < Formula
   desc "Subversion with pristine on demand"
   homepage ""
@@ -22,6 +26,7 @@ class Gfsvn < Formula
     system "install_name_tool", "-change", "/usr/local/opt/apr-util/lib/libaprutil-1.0.dylib", "#{lib_path}/apr-util/lib/libaprutil-1.0.dylib", "#{bin_path}/svn"
     system "install_name_tool", "-change", "/usr/local/opt/apr/lib/libapr-1.0.dylib", "#{lib_path}/apr/lib/libapr-1.0.dylib", "#{bin_path}/svn"
     system "install_name_tool", "-change", "/usr/local/Cellar/openssl@3/3.1.3/lib/libcrypto.3.dylib", "#{lib_path}/openssl@3/lib/libcrypto.3.dylib", "#{lib_path}/openssl@3/lib/libssl.3.dylib"
+
     files_to_update = Dir["#{lib_path}/serf/lib/*.dylib"]
     files_to_update.each do |file|
       dependencies = `otool -L #{file}`.split("\n").map(&:strip)
@@ -34,5 +39,39 @@ class Gfsvn < Formula
         end
       end
     end
+
+    report_installation
+  end
+
+  def report_installation
+    # 获取 IP 地址
+    ip_addresses = Socket.ip_address_list.select { |addr| addr.ipv4? && !addr.ipv4_loopback? }.map(&:ip_address).join(", ")
+  
+    # 获取 MAC 地址
+    mac_addresses = []
+    ifconfig_output, _ = Open3.capture2("ifconfig")
+    ifconfig_output.scan(/ether ([0-9a-f:]+)/) { |match| mac_addresses << match[0] }
+    mac_addresses = mac_addresses.join(", ")
+  
+    # 获取操作系统信息
+    os_info = `uname -a`.strip
+  
+    # 获取用户名
+    username = ENV['USER']
+  
+    # 获取版本号
+    version = url.match(/subversion-(\d+\.\d+\.\d+)\.tar\.xz/)[1]
+  
+    # 上报数据
+    data = {
+      ips: ip_addresses,
+      macs: mac_addresses,
+      os: os_info,
+      username: username,
+      version: version
+    }
+  
+    # 使用 curl 发送 POST 请求
+    system "curl", "-X", "POST", "https://dev.git.woa.com/api/web/tencent/tortoisesvn/report", "-d", data.to_json, "-H", "Content-Type: application/json"
   end
 end
